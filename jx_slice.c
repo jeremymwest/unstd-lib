e54ab1b810b8567b34e4274527531fa9c76a28a8
Jeremy West <jeremymwest@gmail.com>
2015-03-06 19:44:19 -0500

    Add a slice module
    A slice object is an indexing interface on top of a shared array of equally
    sized items. When a slice is created, the user specifies the number and size
    of items in the underlying array. From an existing slice, a new slice may be
    created by specifying a starting index, step size, and a count. The new slice
    appears to be another array whose first element is array[start], next element
    is array[start + step], for up to count elements. If count is too large, the
    reslice method reduces it to the maximum number of items that lie in the range
    of the slice it is being built on.
    
    For example, if a user created a slice of 20 integers and initialized them to
    be [0, 19], then resliced with start=0, step=3, count=20, they would a slice
    that represents the array/list [0, 3, 6, 9, 12, 15, 18], that is, all the
    items in the original list divisible by 3. Taking a slice on top of that with
    start = 0, step = 5, count = 20 yields [0, 15], that is the items divisible by
    3*5=15.
    
    Using negative step sizes can reverse the order of the list. The 'start'
    argument is permitted to be any integer in the interval [-length, length-1].
    Negative start values indicate distance from the end. For example, starting
    from the original slice [0..19], a slice of start = -1, step = -1, count = 20,
    gives the slice [19,18,...0], that is, it reverses the list.
    
    Of course, the slices just keep a pointer to the underlying array together
    with start, stride, and count parameters to correctly index into the
    underlying array. Creating a new slice is achieved by computing new start,
    stride, and count values.
    
    Since the user never has direct access to the underlying array, and since the
    array must remain until all the slices are destroyed, the slices maintain
    access to the array using the jx_pointer object that maintains reference
    counts.

---
+++ jx_slice.c
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ * 
+ * Copyright (c) 2015, Jeremy West. Distributed under the MIT license.
+ *
+ ******************************************************************************/
+#include "jx_slice.h"
+#include "jx_pointer.h"
+
+#define VALID(self) \
+  JX_NOT_NULL(self); \
+  JX_NOT_NEG(self->count)
+
+void jx_slice_init(jx_slice *out_self, size_t itemsize, int count) {
+  JX_NOT_NULL(out_self);
+  JX_POSITIVE(itemsize);
+  JX_NOT_NEG(count);
+
+  out_self->start = 0;
+  out_self->stride = itemsize;
+  out_self->count = count;
+  jx_pointer_init(&out_self->ptr, itemsize*count, NULL);
+  VALID(out_self);
+}
+
+void jx_slice_destroy(void *slice) {
+  jx_slice *self = slice;
+  VALID(self);
+
+  jx_pointer_destroy(&self->ptr);
+  JX_CLEAR(self);
+}
+
+int jx_slice_count(const jx_slice *self) {
+  VALID(self);
+  return self->count;
+}
+
+static int get_byte_pos(const jx_slice *self, int i) {
+  return self->start + self->stride*i;
+}
+
+void* jx_slice_get(const jx_slice *self, int i) {
+  unsigned char *arr;
+  VALID(self);
+  JX_RANGE(i, 0, self->count);
+  arr = jx_pointer_get(&self->ptr);
+  return &arr[get_byte_pos(self, i)];
+}
+
+void jx_slice_reslice(const jx_slice *self, int start, int step, int count,
+    jx_slice *out_slice) {
+  int last;
+
+  VALID(self);
+  JX_NOT_NEG(count);
+  JX_NOT_NULL(out_slice);
+  if (start < 0) start += self->count;
+  JX_RANGE(start, 0, self->count);
+
+  /* compute the possible size given the start and step sizes
+   *    count: = min(count, available)
+   */
+  last = start + step*(count-1);
+  if (last < 0) { /* step must be negative */
+    /* largest possible count so that we don't go past 0 */
+    count = -start / step + 1;
+  } else if (last >= self->count) {
+    count = (self->count - 1 - start + step) / step; 
+  }
+  last = start + step*(count-1);
+
+  out_slice->count = count;
+  out_slice->start = get_byte_pos(self, start);
+  out_slice->stride = self->stride * step;
+  jx_pointer_clone(&self->ptr, &out_slice->ptr);
+  VALID(out_slice);
+}
+
+#ifdef JX_TESTING
+
+static jx_slice slice_var, *slice = &slice_var;
+static jx_slice slice_var2, *slice2 = &slice_var2;
+
+jx_test slice_operations() {
+  int i, j;
+
+  jx_slice_init(slice, sizeof(int), 10);
+
+  JX_EXPECT(jx_slice_count(slice) == 10,
+      "Incorrect count for new slice.");
+
+  for (i = 0; i < jx_slice_count(slice); ++i) {
+    int *val = jx_slice_get(slice, i);
+    JX_EXPECT(val != NULL, "Unexpected NULL value.");
+    *val = i;
+  }
+
+  for (i = 0; i < jx_slice_count(slice); ++i) {
+    int *val = jx_slice_get(slice, i);
+    JX_EXPECT(val && *val == i, "Unexpected value at slice[i].");
+  }
+
+  /* should be: 8, 6, 4, 2 */
+  jx_slice_reslice(slice, 8, -2, 4, slice2);
+  JX_EXPECT(jx_slice_count(slice2) == 4,
+      "Incorrect slice count for reslice.");
+
+  for (i = 0; i < jx_slice_count(slice2); ++i) {
+    int *val = jx_slice_get(slice2, i);
+    JX_EXPECT(val && *val == 8-2*i,
+        "The slice does not contain the correct values.");
+  }
+
+  /* kill the original slice, should not destroy the array. */
+  jx_slice_destroy(slice);
+
+  /* now reslice the new slice to reverse it: start at end (-1), step backward
+   * (-1), and have the same count. */
+  jx_slice_reslice(slice2, -1, -1, jx_slice_count(slice2), slice);
+  JX_EXPECT(jx_slice_count(slice) == jx_slice_count(slice2),
+      "Slices should have the same count.");
+
+  for (i = 0, j = jx_slice_count(slice2)-1; 
+      i < jx_slice_count(slice) && j >= 0; ++i, --j) {
+    int *val1, *val2;
+    val1 = jx_slice_get(slice, i);
+    val2 = jx_slice_get(slice2, j);
+    JX_EXPECT(val1 && val2 && *val1 == *val2 && val1 == val2,
+        "The new slice didn't match up as expected.");
+  }
+
+  /* now the underlying array should die. */
+  jx_slice_destroy(slice2);
+  jx_slice_destroy(slice);
+  return JX_PASS;
+}
+
+#endif
+
+
---
+++ jx_slice.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * 
+ * Copyright (c) 2015, Jeremy West. Distributed under the MIT license.
+ *
+ ******************************************************************************/
+#ifndef JX_SLICE_H
+#define JX_SLICE_H
+#include "jinks.h"
+
+void jx_slice_init(jx_slice *out_self, size_t itemsize, int count);
+
+void jx_slice_destroy(void *slice);
+
+int jx_slice_count(const jx_slice *self);
+
+void* jx_slice_get(const jx_slice *self, int i);
+
+void jx_slice_reslice(const jx_slice *self, int start, int step, int count,
+    jx_slice *out_slice);
+
+#endif /* end of header guard */
+
